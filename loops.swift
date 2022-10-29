e3899d17de7d048d90798d057fc4c377025055d9
Alhay Alhamely <alhayalhamely@Alhays-MacBook-Air.local>
2022-10-29 18:55:01 +0400

    repeat..while

--- loops.swift
+++ loops.swift
@@ -6,14 +6,34 @@ for langauge in langauge{
     print(langauge)
 }
 
-//for loop (where clause)
 
+
+
+//for loop (where clause)
 // use the (where) in for...loop to implent filters in the loop
 for Languages in langauge where Languages != "English"
 {
     print(Languages)
 }
 
+
+
 // for..loop -> using range in statement
+//in the print funtion where the 2 is the starting point in the list
+let name = ["alhai","ahmed","Hamad","alhamely"]
+print(name[2...])
 
 
+
+
+
+
+ for..loop -> with stride function-> stride(from: ,to: ,by: )
+ from->is the startig point
+   to->is the end point
+   by-> number of skips
+
+
+for i in stride(from:10, to: 200, by: 4){
+    print(i)
+}
---
+++ while.swift
@@ -0,0 +1,15 @@
+// while loop
+var i = 0
+
+while (i < 200)
+{
+    if(i % 2 == 1){
+        print(i)
+    }
+    i += 1
+}
+
+//repeat..while Loop
+
+
+
